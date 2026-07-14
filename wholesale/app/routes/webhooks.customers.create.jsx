3e0fbd6f8971de0cfd3e33bbef21281b7d00011a
ramkumawat-ITG <ramkumawat@itgeeks.com>
2026-07-14 12:59:55 +0530

    wholesale build error

--- wholesale/app/routes/webhooks.customers.create.jsx
+++ wholesale/app/routes/webhooks.customers.create.jsx
@@ -12,8 +12,6 @@ import {
 } from "../services/shopify/shopify.mutations";
 import { createLogger } from "../utils/logger.utils";
 import connectDB from "../services/APIService/mongo.service";
-import WholesaleApplication from "../models/wholesaleApplication.server";
-import { syncPractitionerToRetail } from "../services/retailSync/practitioner.service";
 import { dropshipConfig } from "../services/dropship/dropship.config";
 
 const log = createLogger("webhook.customers_create");
