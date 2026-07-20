b85d690250c44360ac1c47055aa66a2d21d7a797
Durgesh Selkari <durgeshselkari@itgeeks.com>
2026-07-20 20:33:36 +0530

    URL updated

--- ns-retail/app/routes/webhooks.orders.create.jsx
+++ ns-retail/app/routes/webhooks.orders.create.jsx
@@ -117,7 +117,7 @@ export async function action({ request }) {
 async function forwardToWholesaleDropship({ payload, retailShop }) {
   // eslint-disable-next-line no-undef
   const apiBase =
-    "https://ns-wholesale.apps.itgeeksin.com" ||
+    "https://natural-solutions-wholesale.onrender.com" ||
     process.env.WHOLESALE_API_BASE;
   // eslint-disable-next-line no-undef 
   const wholesaleShop = process.env.WHOLESALE_SHOP;
